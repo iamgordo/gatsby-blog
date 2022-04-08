f467b52f37fad241cea8e26229ee76bb2d74e490
Gordon McKenn <medicnumberone@aim.com>
2022-04-08 10:51:32 -0400

    4th

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./layout.module.css";
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <title className={heading}>{pageTitle}</title>
+      <nav>
+        <ul>
+          <li>
+            <Link className={navLinks} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
